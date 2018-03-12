e604135c0634a2dca144c4edd29a4155ee097a49
Artem <35493226+ArtemDrushchyts@users.noreply.github.com>
2018-03-12 21:03:45 +0300

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -43,6 +43,7 @@ h1, h2, h3, h4, h5, h6{margin-bottom:1.1em;}
 	<link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
 	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
 	<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
+	<link rel="stylesheet" href="animate.css">
 </head>
 
 <body>
